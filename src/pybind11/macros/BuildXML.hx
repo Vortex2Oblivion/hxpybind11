7644ab539385ecf892ef6a97ca192f6aea9284d1
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-12-19 22:39:10 -0500

    gonna work on tomorrow prob

--- src/pybind11/macros/BuildXML.hx
+++ src/pybind11/macros/BuildXML.hx
@@ -14,12 +14,12 @@ class BuildXML {
             <include name='${haxelib:hxpybind11}/build.xml' />
             
 	
-            <target id='haxe' tool='linker' toolid='exe' >
-                <lib name='__PYTHON__PATH__' />
+            <target id='haxe'>
+                <lib name='__PYTHON__LIB__FILE__' />
             </target>
         \")";
 
-        var process:Process = new Process('where', ['python']);
+        /*var process:Process = new Process('where', ['python']);
         if (process.exitCode() != 0) {
 			var message:String = process.stderr.readAll().toString();
 			var pos:Position = Context.currentPos();
@@ -39,6 +39,41 @@ class BuildXML {
         }
 
         buildXML = buildXML.replace('__PYTHON__PATH__', foundPath.replace("python.exe", 'libs/python315.lib'));
+        buildXML = buildXML.replace("\\", "/");*/
+
+        var process:Process = new Process('haxelib', ['libpath', 'hxpybind11']);
+        if (process.exitCode() != 0) {
+			var message:String = process.stderr.readAll().toString();
+			var pos:Position = Context.currentPos();
+            process.close();
+			Context.error(message, pos);
+		}
+
+
+        var libpath:String = process.stdout.readLine();
+        process.close();
+
+        var oldDir:String = Sys.getCwd();
+
+        var process:Process = new Process('haxelib', ['libpath', 'hxpybind11']);
+        if (process.exitCode() != 0) {
+			var message:String = process.stderr.readAll().toString();
+			var pos:Position = Context.currentPos();
+            process.close();
+			Context.error(message, pos);
+		}
+
+        try{
+        Sys.command('cd $libpath/external/cpython');
+        Sys.command('./configure --with-pydebug');
+        Sys.command('make');
+        }
+        catch(e:Dynamic){
+        }
+        Sys.command('cd $oldDir');
+
+        
+        buildXML = buildXML.replace('__PYTHON__LIB__FILE__', "${cpython_folder}/libpython3.15d.a");
         buildXML = buildXML.replace("\\", "/");
 
 		Compiler.addGlobalMetadata('pybind11', buildXML);
