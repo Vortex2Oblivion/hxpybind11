e87f3cd15699c227a415c731e4b3ef1dff150790
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-12-21 14:49:32 -0500

    yeah

--- src/pybind11/ScopedInterpreter.hx
+++ src/pybind11/ScopedInterpreter.hx
@@ -1,7 +1,11 @@
 package pybind11;
 
+import cpp.ConstCharStar;
+import cpp.RawConstPointer;
+
 @:include("pybind11/embed.h")
 @:native("pybind11::scoped_interpreter")
 extern class ScopedInterpreter {
+    @:overload(function(initSignalHandlers:Bool = true, argc:Int = 0, argv:RawConstPointer<ConstCharStar> = cast 0, addProgramDirToPath:Bool = true):Void{})
     function new():Void;
 }
--- src/pybind11/macros/BindModule.hx
+++ src/pybind11/macros/BindModule.hx
@@ -1,16 +1,17 @@
 package pybind11.macros;
 
-import eval.luv.Stream;
-import haxe.macro.Compiler;
+#if macro
 import haxe.macro.Context;
 import haxe.macro.Expr;
 import haxe.macro.Type;
 import haxe.macro.ExprTools;
 
 using StringTools;
+#end
 
 class BindModule {
 	private static function packageToNamepsace(pack:Array<String>) {
+		#if macro
 		if (pack.length == 0)
 			return "";
 		var namespaceString:String = "";
@@ -18,13 +19,15 @@ class BindModule {
 			namespaceString += p + "::";
 		}
 		return namespaceString;
+		#end
 	}
 
 	// riconuts my goat thank you so much for this
 	public static macro function bindModules():Array<Field> {
+		#if macro
 		var localClass = Context.getLocalClass();
 		var cl:ClassType = localClass.get();
-		var clMeta = cl.meta;
+		var clMeta:MetaAccess = cl.meta;
 
 		var outputCode:String = clMeta.extract(':cppFileCode').length > 0 ? ExprTools.getValue(clMeta.extract(':cppFileCode')[0].params[0]) : "";
 
@@ -33,20 +36,20 @@ class BindModule {
 
 			var funcs:Array<Array<String>> = ExprTools.getValue(meta.params[1]);
 
-			var cppFuncDefs = "";
+			var cppFuncDefs:String = "";
 
 			for (funcData in funcs) {
-				var split = funcData[0].split('.');
+				var split:Array<String> = funcData[0].split('.');
 
-				var funcName = split[split.length - 1];
+				var funcName:String = split[split.length - 1];
 
-				var className = cl.name;
+				var className:String = cl.name;
 
 				if (funcName.split('.').length >= 1 && split[0] != funcName) {
 					className = split[0];
 				}
 
-				var funcDesc = funcData[1];
+				var funcDesc:String = funcData[1];
 
 				cppFuncDefs += 'm.def("$funcName", &${packageToNamepsace(cl.pack)}${className}_obj::$funcName, "$funcDesc");\n';
 			}
@@ -80,5 +83,6 @@ class BindModule {
 		}
 
 		return Context.getBuildFields();
+		#end
 	}
 }
